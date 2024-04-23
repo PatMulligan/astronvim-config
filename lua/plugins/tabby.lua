6c2819f849f356721617ed4b05eec9d3b0c8b60e
Patrick Mulligan <patjmulligan@protonmail.com>
2024-04-23 17:03:59 -0600

    rever to lazy = false

--- lua/plugins/tabby.lua
+++ lua/plugins/tabby.lua
@@ -1,6 +1,11 @@
 return {
   "TabbyML/vim-tabby",
-  -- event = "User AstroFile",
+  -- event = "VeryLazy",
+  -- event = "User Astrofile",
+  -- The two above were not working, however mehalter says `lazy = false` is definitely better than nothing!
+  -- Some vim plugins struggle with lazy loading
+  -- Definitely better to do that than have bad loading
+  lazy = false, -- specifies that the plugin should be loaded at Neovim's startup rather than being loaded lazily
   dependencies = {
     "AstroNvim/astrocore",
     ---@type AstroCoreOpts
