e5b4f97c6659da2d9ab17c7cfa007d06b8058be7
Patrick Mulligan <patjmulligan@protonmail.com>
2024-05-11 00:04:24 -0600

    make splash pastels

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,13 +1,20 @@
 return {
   "goolord/alpha-nvim",
   opts = function(_, opts) -- override the options using lazy.nvim
-    vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#DA4939" })
-    vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#FF875F" })
-    vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = "#FFC66D" })
-    vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = "#00FF03" })
-    vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = "#00AFFF" })
-    vim.api.nvim_set_hl(0, "NeovimDashboardLogo6", { fg = "#8800FF" })
+    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#DA4939" })
+    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#FF875F" })
+    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = "#FFC66D" })
+    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = "#00FF03" })
+    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = "#00AFFF" })
+    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo6", { fg = "#8800FF" })
 
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#FFB6C1" })
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#E6E6FA" })
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = "#98FF98" })
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = "#87CEEB" })
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = "#FFE5B4" })
+    vim.api.nvim_set_hl(0, "NeovimDashboardLogo6", { fg = "#FADADD" })
+    --
     -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#DA4939" })  -- Reddish Brown              
     -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#FF875F" })  -- Light Coral                
     -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = "#FFC66D" })  -- Pale Goldenrod             
@@ -57,13 +64,6 @@ return {
     -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = "#FF00FF" })  
     -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = "#87CEEB" })  
     -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo6", { fg = "#E75480" })  
-    --
-    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#FFB6C1" })  
-    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#E6E6FA" })  
-    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = "#98FF98" })  
-    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = "#87CEEB" })  
-    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = "#FFE5B4" })  
-    -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo6", { fg = "#FADADD" })  
     --
     -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#39FF14" })  
     -- vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#FF1493" })  
