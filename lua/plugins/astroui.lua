f5fe9b95b0109c7f6d58e9e6c594096089e18ef2
Patrick Mulligan <patjmulligan@protonmail.com>
2024-05-11 00:04:15 -0600

    change colorscheme back to catppuccin

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -9,7 +9,7 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "cyberdream", -- kanagawa", -- "catppuccin", -- "astrodark",
+    colorscheme =  "catppuccin", -- "cyberdream", -- kanagawa", -- "astrodark",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
